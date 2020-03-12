cbc27356f076db2fc87de8f04de613b7120711d9
tbaustin <tbaustin1992@gmail.com>
2020-03-12 13:43:06 -0500

    update babel plugins

--- packages/babel-preset/index.js
+++ packages/babel-preset/index.js
@@ -26,9 +26,34 @@ module.exports = declare((api, options) => { // declare function allows better e
 		], // presets that include most of the plugins needed
 		plugins: [
 			require(`@babel/plugin-transform-runtime`),
+			require(`@babel/plugin-proposal-function-bind`),
+			require(`@babel/plugin-proposal-export-default-from`),
+			require(`@babel/plugin-proposal-logical-assignment-operators`),
+			[require(`@babel/plugin-proposal-optional-chaining`), {
+				loose: false,
+			}],
+			[require(`@babel/plugin-proposal-pipeline-operator`), {
+				proposal: `minimal`,
+			}],
+			[require(`@babel/plugin-proposal-nullish-coalescing-operator`), {
+				loose: false,
+			}],
+			require(`@babel/plugin-proposal-do-expressions`),
+			[require(`@babel/plugin-proposal-decorators`), {
+				legacy: true,
+			}],
+			require(`@babel/plugin-proposal-function-sent`),
+			require(`@babel/plugin-proposal-export-namespace-from`),
+			require(`@babel/plugin-proposal-numeric-separator`),
+			require(`@babel/plugin-proposal-throw-expressions`),
+			require(`@babel/plugin-syntax-dynamic-import`),
+			require(`@babel/plugin-syntax-import-meta`),
 			[require(`@babel/plugin-proposal-class-properties`), {
 				loose: false,
 			}],
+			require(`@babel/plugin-proposal-json-strings`),
+			require(`babel-plugin-dynamic-import-node`),
+			require(`@babel/plugin-proposal-object-rest-spread`),
 		], // plugins not included in presets
 	}
 })
